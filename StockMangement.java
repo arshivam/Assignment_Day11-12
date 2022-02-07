38285936810c6c02521473c7d36ea4b71e33dca2
arshivam <shivam.armarkar007@gmail.com>
2022-02-07 17:46:31 +0530

    Added : Stock management code

--- StockMangement.java
+++ StockMangement.java
@@ -2,7 +2,7 @@ package com.bridgelabz.day11_12;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class Stocks{
+class Stock{
 	String ShareName;
 	int noOfShares;
 	int sharePrice;
@@ -26,33 +26,74 @@ class Stocks{
 		this.sharePrice = sharePrice;
 	}
 }
-class StockPortfolio extends Stocks {
-	ArrayList<Stocks> portfolio = new ArrayList<>();
+class StockPortfolio {
+	ArrayList<Stock> portfolio = new ArrayList<>();
 	double totalValue = 0;
+	int counter = 0;
 
-	public double totalValue() {
-		for (Stocks stock : portfolio) {
-			totalValue += stock.sharePrice * stock.noOfShares;
-		}
-		return totalValue;
-	}
-}
-public class StockMangement {
-	public static void addStock() {
-		StockPortfolio list = new StockPortfolio();
+	public void addStock() {
+		Stock stockInfo = new Stock();
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter details of Which Stock You Want to Purchase : ");
 		System.out.println("Enter Share Name : ");
-		list.setShareName(sc.next());
+		stockInfo.setShareName(sc.next());
 		System.out.println("Enter Number of Shares : ");
-		list.setNoOfShares(sc.nextInt());
+		stockInfo.setNoOfShares(sc.nextInt());
 		System.out.println("Enter Share Price : ");
-		list.setSharePrice(sc.nextInt());			
-		}
-	public static void main(String[] args) {
-		StockMangement a = new StockMangement();
-		a.addStock();
+		stockInfo.setSharePrice(sc.nextInt());	
+
+		portfolio.add(stockInfo);
 		System.out.println();
+		System.out.println("Stock Added");
+		counter++;
 	}
+
+	public void addNoOfStocks() {
+		System.out.println("Enter the number of Stocks you want to Purshase");
+		Scanner sc = new Scanner(System.in);
+		int noOfStocks = sc.nextInt();
+		for (int i=1;i<=noOfStocks;i++) {
+			addStock();
+		}
 	}
+
+	public void totalValue() {
+		for (Stock stock : portfolio) {
+			totalValue += stock.sharePrice * stock.noOfShares;
+		}
+		System.out.println(totalValue);
+	}
+}
+
+public class StockMangement {
+	static StockPortfolio a = new StockPortfolio();
+	public static void main(String[] args) {
+
+		int choice;
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter your choice \n1: Add Stock \n2: Add Multiple of Stocks "
+					           + "\n3: Display Total value of Stocks \n0: Exit ");
+			choice = sc.nextInt();
+
+			switch (choice) {
+			case 1:
+				a.addStock();
+				break;
+			case 2:
+			    a.addNoOfStocks();
+				break;
+			case 3:
+				System.out.println("Total value of your stocks is : ");
+				a.totalValue();
+				break;
+			default:
+				System.out.println("Exit");
+				choice = 3;
+			}
+		}
+		while (choice < 4);
+	}
+}
 
